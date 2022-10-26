4ada9f062412e5246631db714173cdd2c24f0ffe
Gu-Miao <1450206741@qq.com>
2022-10-26 14:11:29 +0800

    Add comments for to function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,9 @@
+/**
+ * A utility function to wrap a promise and make it returns a tuple
+ * with error occured or result it keep
+ * @param promise The promise to wrap
+ * @returns The tuple carring error or result
+ */
 export function to<T>(promise: Promise<T>): Promise<[null, T] | [any]> {
   return new Promise((reslove, reject) => {
     promise
