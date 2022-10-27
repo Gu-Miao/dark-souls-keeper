3399d6dae43d7fa2a3f895c8f8e500ee189e0a0c
Gu-Miao <1450206741@qq.com>
2022-10-27 09:50:03 +0800

    Fix seplling error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 /**
  * A utility function to wrap a promise and make it returns a tuple
- * with error occured or result it keep
+ * with error occured or result it keeps
  * @param promise The promise to wrap
  * @returns The tuple carring error or result
  */
